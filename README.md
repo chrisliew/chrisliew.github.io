b9dd0d902873772d9dedb4105f1fd2def5e598df
chrisliew <chriswvliew@gmail.com>
2019-02-02 02:04:19 +0800

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-Developer portfolio for Christopher Liew
-
-Hosted here: https://chrisliew.github.io/
-
-Built with React
---
+++ precache-manifest.d9f8d7b246730fd22f436ab1bf7f67eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f5bf1522f2a28ca4a09c",
+    "url": "/static/js/main.f5bf1522.chunk.js"
+  },
+  {
+    "revision": "e8e1044146f97a8b46f7",
+    "url": "/static/js/1.e8e10441.chunk.js"
+  },
+  {
+    "revision": "f5bf1522f2a28ca4a09c",
+    "url": "/static/css/main.7de61886.chunk.css"
+  },
+  {
+    "revision": "6849f5a2f2a347c7924a217c04759f3e",
+    "url": "/index.html"
+  }
+];
